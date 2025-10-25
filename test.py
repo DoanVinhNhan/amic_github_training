79484ead48a9921dd5b0ce8ebcd61362edaa8e71
Doanvinhnhan24 <Nhan.DV237376@sis.hust.edu.vn>
2025-10-25 20:17:39 +0700

    Commit 2

---
+++ test.py
@@ -0,0 +1,9 @@
+# Nhập hai số từ người dùng
+a = float(input("Nhập số a: "))
+b = float(input("Nhập số b: "))
+
+# Tính tích
+ketqua = a * b
+
+# In kết quả
+print(f"Tích của {a} và {b} là: {ketqua}")
